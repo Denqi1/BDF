90ab35fec39b4255164412cdf9955a576b836adf
dskorik <dskorik@team.amocrm.com>
2024-09-21 23:46:41 +0300

    return variable app to lower case

--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@ import express from "express";
 import { connect } from "mongoose";
 import { router } from "./routes.js";
 
-const APP = express();
+const app = express();
 const PORT = 3000;
 const URL = "mongodb://localhost:27017/main";
 
 connect(URL);
 
-APP.use(express.json(), router);
+app.use(express.json(), router);
 
-APP.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("server is running, port: " + PORT);
 });
 
-export { APP };
+export { app };
