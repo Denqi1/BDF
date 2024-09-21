ffc5d47caed810d8230e37c49aa4080bbc64454f
dskorik <dskorik@team.amocrm.com>
2024-09-21 23:41:22 +0300

    change const variables to upper case

--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@ import express from "express";
 import { connect } from "mongoose";
 import { router } from "./routes.js";
 
-const app = express();
-const port = 3000;
-const url = "mongodb://localhost:27017/main";
+const APP = express();
+const PORT = 3000;
+const URL = "mongodb://localhost:27017/main";
 
-connect(url);
+connect(URL);
 
-app.use(express.json(), router);
+APP.use(express.json(), router);
 
-app.listen(port, () => {
-  console.log("server is running, port: " + port);
+APP.listen(PORT, () => {
+  console.log("server is running, port: " + PORT);
 });
 
-export { app };
+export { APP };
